c83f243671f4845b547ad6b6983eaa41abe7c5bd
luo0412 <1095847440@qq.com>
2019-04-29 09:20:48 +0800

    small change

--- vuecli3-flexible-cubeui-vantui-wangyupo-simple-h5/vue.config.js
+++ vuecli3-flexible-cubeui-vantui-wangyupo-simple-h5/vue.config.js
@@ -5,6 +5,7 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
+
 module.exports = {
     // 使用运行时编译器的 Vue 构建版本
     runtimeCompiler: true,
@@ -16,10 +17,14 @@ module.exports = {
     lintOnSave: false,
 
     devServer: {
-        open: false,        // 是否自动打开浏览器页面
-        host: '0.0.0.0',    // 指定使用一个 host，默认是 localhost
-        port: 8080,         // 端口地址
-        https: false,       // 使用https提供服务
+        // 是否自动打开浏览器页面
+        open: false,
+        // 指定使用一个 host，默认是 localhost
+        host: '0.0.0.0',
+        // 端口地址
+        port: 8080,
+        // 使用https提供服务
+        https: false,
         // 设置代理，此处应该配置为开发服务器的后台地址
         proxy: 'http://api.zhuishushenqi.com'
     },
